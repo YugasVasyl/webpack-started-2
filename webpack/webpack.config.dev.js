4b710c63b5902cb148eeca2aef9e6bf9279b3dd3
YugasVasyl <drakka@meta.ua>
2019-02-25 20:20:20 +0200

    add files

---
+++ webpack/webpack.config.dev.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const Webpack = require('webpack');
+const Path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const dest = Path.join(__dirname, '../dist');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'cheap-eval-source-map',
+  devServer: {
+    contentBase: dest,
+    inline: true
+  },
+  plugins: [
+    new Webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    }),
+    new CopyWebpackPlugin([
+      { from: 'src/assets', to: 'assets' }
+    ])
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        include: Path.resolve(__dirname, '../src'),
+        enforce: "pre",
+        loader: 'eslint-loader',
+        options: {
+          emitWarning: true,
+        }
+      },
+      {
+        test: /\.(js)$/,
+        include: Path.resolve(__dirname, '../src'),
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.s?css$/i,
+        use: ['style-loader', 'css-loader?sourceMap=true', 'sass-loader']
+      },
+      {
+        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]'
+          }
+        }
+      }
+    ]
+  }
+});
